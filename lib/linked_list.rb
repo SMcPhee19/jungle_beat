56719d3601ca3cc26f0083146f82f39fda9762c6
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-02 20:38:40 -0700

    Finish iteration 1

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -6,14 +6,34 @@ class LinkedList
   end
 
   def append(sound)
-    @head = Node.new(sound)
+    if self.count == 0
+      @head = Node.new(sound)
+    else
+      @head.next_node = Node.new(sound)
+    end
     sound
   end
 
   def count
-    if @head == nil
-      0
+    return 0 if @head == nil
+    node_number = 1
+    current_node = @head
+    while current_node.next_node != nil
+      current_node = current_node.next_node
+      node_number += 1
     end
+    node_number
+  end
+
+  def to_string
+    return "" if @head == nil
+    current_node = @head
+    sounds = [current_node.data]
+    while current_node.next_node != nil
+      current_node = current_node.next_node
+      sounds << current_node.data
+    end
+    sounds.join(" ")
   end
 
 end
