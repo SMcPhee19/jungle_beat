ec78b83c0cb36c14aa42a179140ecb9d4f6ff340
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-02 20:38:22 -0700

    Finish iteration 1

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -2,18 +2,29 @@ require 'rspec'
 require './lib/node'
 require './lib/linked_list'
 
-RSpec.describe 'linked_list' do
+RSpec.describe 'linked_list' do 
+  let(:list) {LinkedList.new}
+
   it 'has list' do
-    list = LinkedList.new
-    
-    
     expect(list.head).to be nil
     expect(list.append("doop")).to eq("doop")
     expect(list.head.next_node).to be nil
     expect(list.head.data).to eq("doop")
     expect(list.count).to eq(1)
     expect(list.to_string).to eq("doop")
-    # require 'pry'; binding.pry
+  end
+
+  it 'has multiple nodes' do
+    expect(list.head).to be nil
+    expect(list.append("doop")).to eq("doop")
+    expect(list.head.next_node).to be nil
+
+    list.append("deep")
+
+    expect(list.head.next_node).to be_a Node
+    expect(list.head.next_node.data).to eq("deep")
+    expect(list.count).to eq(2)
+    expect(list.to_string).to eq("doop deep")
   end
 
 
