0ca5ffb14681fa094a842211a6fe27066d74553a
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-04 17:05:47 -0700

    Add can include? and can pop tests

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -25,21 +25,49 @@ RSpec.describe 'linked_list' do
     expect(list.head.next_node.data).to eq("deep")
     expect(list.count).to eq(2)
     expect(list.to_string).to eq("doop deep")
-
-    # require 'pry'; binding.pry
-
   end
 
   it 'can insert' do
     list.append("doop")
     list.append("deep")
     list.append("woo")
-    require 'pry'; binding.pry
 
     expect(list.insert(1, "foo")).to eq("foo")
     expect(list.to_string).to eq("doop foo deep woo")
-
   end
 
+  it 'can find' do
+    list.append("deep")
+    list.append("woo")
+    list.append("shi")
+    list.append("shu")
+    list.append("blob")
+
+    expect(list.find(2, 1)).to eq ("shi")
+    expect(list.find(1, 3)).to eq("woo shi shu")
+  end
+
+  it 'can include?' do
+    list.append("deep")
+    list.append("woo")
+    list.append("shi")
+    list.append("shu")
+    list.append("blob")
+    
+    expect(list.includes?("deep")).to be true 
+    expect(list.includes?("dep")).to be false
+  end
+
+  it 'can pop' do
+    list.append("deep")
+    list.append("woo")
+    list.append("shi")
+    list.append("shu")
+    list.append("blob")
+
+    expect(list.pop).to eq("blop")
+    expect(list.pop).to eq("shu")
+    expect(list.to_string).to eq("dee woo shi")
+  end
 
 end
