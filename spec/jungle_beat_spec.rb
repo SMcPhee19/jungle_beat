0a94f9e8f5ce32cf55bf6529dea54fd64ae8941e
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-05 15:52:33 -0700

    Initial commit

---
+++ spec/jungle_beat_spec.rb
@@ -0,0 +1,35 @@
+require 'rspec'
+require "./lib/jungle_beat"
+require "./lib/linked_list"
+require "./lib/node"
+
+RSpec.describe 'jungle_beat' do 
+  let(:jb) {JungleBeat.new}
+
+  
+  
+  xit 'has list' do
+    
+
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
