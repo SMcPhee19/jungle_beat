857830a9097fe8cb965aaefc01bd302a8f0344bf
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-05 16:54:26 -0700

    Add has list test

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -6,13 +6,18 @@ require "./lib/node"
 RSpec.describe 'jungle_beat' do 
   let(:jb) {JungleBeat.new}
 
-  
-  
-  xit 'has list' do
-    
-
-
+  it 'has list' do
+    # require 'pry'; binding.pry
+    expect(jb.list).to be_a(LinkedList)
   end
+
+  it 'can append' do
+    jb.append("deep doo ditt")
+
+    expect(jb.append("deep doo dit")) to eq("deep doo dit")
+  end
+  
+    
 
 
 
