834967456b86eb9e5dba3f15dbf955504884783c
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-05 16:54:50 -0700

    Add initialize

---
+++ lib/jungle_beat.rb
@@ -0,0 +1,27 @@
+class JungleBeat
+
+  attr_accessor :list
+  def initialize()
+    @list = LinkedList.new
+  end
+  
+
+  def append(sound)
+    sound = []
+
+    sounds << 
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
