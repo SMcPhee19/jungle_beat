ad3013eacce9fcb99acffffba00f20aed52467af
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-05 17:19:52 -0700

    Add play method

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -2,20 +2,25 @@ class JungleBeat
 
   attr_accessor :list
   def initialize()
-    @list = LinkedList.new
+    @list = LinkedList.new  ## list instance variable is a new LinkedList instance
   end
   
 
   def append(sounds)
-    sounds.split(" ").each do |sound|
-      list.append(sound)
+    sounds.split(" ").each do |sound|  ## taking the sounds argument, splitting them and then itterating through each sound
+      list.append(sound) ## calling the list.append method from the linked_list
     end
-    sounds
+    sounds ## returning sounds
   end
 
   def count
-    list.count
+    list.count ## calling the list.count method from the linked_list methods 
   end
+
+  def play
+    `say -r 500 -v Boing #{list.to_string}`  ## the data is stored in the linked_list using the .to_string to return the sounds
+  end
+end
 
   
 
@@ -27,5 +32,3 @@ class JungleBeat
 
 
 
-
-end
