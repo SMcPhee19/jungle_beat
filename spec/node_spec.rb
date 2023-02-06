856e5d6aa13861b8465c2bd4d0167c53516869a9
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-05 22:12:55 -0700

    remove pry

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -5,8 +5,7 @@ RSpec.describe 'nodes' do
   
   it 'has nodes' do
     node = Node.new("plop")
-    
-    require 'pry'; binding.pry
+     
     expect(node.data).to eq("plop")
     expect(node.next_node).to be nil
   end
