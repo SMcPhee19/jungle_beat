a8fe7bd0b4809ca5452592a77f2b1fb4ad448072
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-05 17:08:33 -0700

    add append and count tests

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -7,16 +7,20 @@ RSpec.describe 'jungle_beat' do
   let(:jb) {JungleBeat.new}
 
   it 'has list' do
-    # require 'pry'; binding.pry
     expect(jb.list).to be_a(LinkedList)
   end
 
   it 'can append' do
     jb.append("deep doo ditt")
-
-    expect(jb.append("deep doo dit")) to eq("deep doo dit")
+    expect(jb.append("deep doo dit")).to eq("deep doo dit")
   end
   
+  it 'can count' do
+    jb.append("deep doo ditt")
+    jb.append("woo hoo shu")
+    require 'pry'; binding.pry
+    expect(jb.count).to eq(6)
+  end
     
 
 
